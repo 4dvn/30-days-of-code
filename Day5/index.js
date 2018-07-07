02be53854ad7023983cb6042896c801ce3f69def
Ian McLinden <mcli0027@umn.edu>
2018-07-06 22:33:39 -0500

    added day 5

---
+++ Day5/index.js
@@ -0,0 +1,219 @@
+'use strict';
+var https = require('https');
+
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
+ * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
+ * testing instructions are located at http://amzn.to/1LzFrj6
+ *
+ * For additional samples, visit the Alexa Skills Kit Getting Started guide at
+ * http://amzn.to/1LGWsLG
+ */
+
+function httpsGet(query, callback) {
+    var options = {
+        host: 'owls-in.space',
+        path: '/ghost/api/v0.1/' + query,
+        method: 'GET',
+        port: 443
+    };
+
+    var req = https.request(options, res => {
+        console.log('a cat anus');
+        res.setEncoding('utf8');
+        var dataline = '';
+        //accept incoming data asynchronously
+        res.on('data', chunk => {
+            dataline += chunk;
+        });
+
+        //return the data when streaming is complete
+        res.on('end', () => {
+            try {
+                var responseJSON = JSON.parse(dataline);
+            } catch (err) {
+                console.log("ERROR ==== Invalid JSON received");
+                return;
+            }
+            callback(responseJSON);
+        });
+
+    });
+    req.end();
+}
+
+// --------------- Helpers that build all of the responses -----------------------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: 'PlainText',
+            text: output,
+        },
+        card: {
+            type: 'Simple',
+            title: `SessionSpeechlet - ${title}`,
+            content: `SessionSpeechlet - ${output}`,
+        },
+        reprompt: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: repromptText,
+            },
+        },
+        shouldEndSession,
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
+}
+
+
+// --------------- Functions that control the skill's behavior -----------------------
+
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    const sessionAttributes = {};
+    const cardTitle = 'Welcome';
+    const speechOutput = 'Welcome to thirty days of code.' +
+        'Try asking about the progress of an author by asking thirty days of code how many articles that author has.';
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    const repromptText = '';
+    const shouldEndSession = true;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleSessionEndRequest(callback) {
+    const cardTitle = 'Session Ended';
+    const speechOutput = 'Check up on thirty days of code again';
+    // Setting this to true ends the session and exits the skill.
+    const shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+function getArticlesByAuth(intent, session, callback) {
+    const repromptText = null;
+    const sessionAttributes = {};
+    let shouldEndSession = true;
+    let speechOutput = '';
+
+    let author = intent.slots.Author.value;
+
+    let query = `posts/?client_id=ghost-frontend&client_secret=123257bef36d&include=authors&filter=authors:[${author}]`;
+
+    httpsGet(query, (theResult) => {
+        console.log("sent     : " + query);
+        console.log("received : " + theResult);
+
+        let articleCount = theResult.posts.length;
+
+        if (articleCount) {
+            speechOutput = `${author} has ${articleCount} currently published articles.`;
+            shouldEndSession = true;
+        } else {
+            speechOutput = `I cant find any articles by ${author}.`;
+        }
+
+        // Setting repromptText to null signifies that we do not want to reprompt the user.
+        // If the user does not respond or says something that is not understood, the session
+        // will end.
+        callback(sessionAttributes,
+            buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+    });
+
+}
+
+
+// --------------- Events -----------------------
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
+}
+
+/**
+ * Called when the user launches the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
+
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
+
+    const intent = intentRequest.intent;
+    const intentName = intentRequest.intent.name;
+
+    // Dispatch to your skill's intent handlers
+    if (intentName === 'articles') {
+        getArticlesByAuth(intent, session, callback);
+    } else if (intentName === 'AMAZON.HelpIntent') {
+        getWelcomeResponse(callback);
+    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+        handleSessionEndRequest(callback);
+    } else {
+        throw new Error('Invalid intent');
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
+    // Add cleanup logic here
+}
+
+
+// --------------- Main handler -----------------------
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = (event, context, callback) => {
+    try {
+        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+
+        if (event.session.new) {
+            onSessionStarted({
+                requestId: event.request.requestId
+            }, event.session);
+        }
+
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
+            onIntent(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
+        }
+    } catch (err) {
+        callback(err);
+    }
+};
