747a56ef34b9ed451c2b81f4e2318fdbc7e6dd1d
Ian McLinden <mcli0027@umn.edu>
2018-07-08 00:34:02 -0500

    Added Day 6

---
+++ Day6/am.c
@@ -0,0 +1,36 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void beep(){
+	for(char i=50;i;i--){
+		PORTB|=_BV(PB0);_delay_ms(1);
+		PORTB&=~_BV(PB0);_delay_ms(1);
+	}
+}
+
+void rest(){_delay_ms(100);}
+
+void dot(){beep();rest();}
+void dash(){beep();beep();beep();rest();}
+void space(){rest();rest();}
+void space2(){space();space();}
+
+int main(){
+	DDRB|=_BV(PB0);
+	for(;;){
+		dot();dot();dot();space(); 			// S
+		dash();dot();dash();dot();space(); 	// C
+		dash();dash();dash();space(); 		// O
+		dash();space(); 					// T
+		dash();space(); 					// T
+		space2();
+		dot();dash();dot();space();			// R
+		dash();dash();dash();space(); 		// O
+		dash();dot();dash();dot();space(); 	// C
+		dash();dot();dash();space();		// K
+		dot();dot();dot();space(); 			// S
+		_delay_ms(1000); // silence
+	}
+	return 0;
+}
